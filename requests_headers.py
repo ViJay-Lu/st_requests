9edcf29a0852109b601356e853679705889f140c
ViJay <1047527105@qq.com>
2024-10-16 14:44:49 +0800

    st_request code

--- requests_headers.py
+++ requests_headers.py
@@ -11,7 +11,7 @@ query_params = {
 headers = {
     "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
 }
-#不加headers参数，请求报错418
+#不加headers参数， 请求报错418
 rsp = requests.get(url, params=query_params,headers=headers)
 print(rsp.status_code)
 print(rsp.json())
